a530cd11c67f49f6fce5ecb9db0670abd3c8cab6
MatrixField <david.xelnaga@live.com>
2015-10-01 02:02:46 -0700

    Add Method2 for Problem. Tested. -- MatrixField

--- ProjectEulerSolutions/Program.fs
+++ ProjectEulerSolutions/Program.fs
@@ -10,12 +10,10 @@
 
 let MAX = 1000 //Constant to store the max
 
-//*************************************************************************************************
+//************************************************************************************************
 
-// Method 1:
-// naive search
 
-module Method1 =
+module Method1 = // naive search
     begin
 
     //Function to fetch a list of all multiples of "value" ranges from [0, upperLimit)
@@ -31,8 +29,8 @@ open System.Linq
         let list2 = searchMultiplesOf 5 MAX
         let list3 = list1.Intersect list2
 
-        let outList list =
-            list|>List.map (fun x-> printfn "%A\n" x)
+        //let outList list =
+        //    list|>List.map (fun x-> printfn "%A\n" x)
 
         //outList list1
         //outList list2
@@ -40,10 +38,30 @@ open System.Linq
 
         (list1.Sum(fun x->x) + list2.Sum(fun x->x) - list3.Sum(fun x->x))
     end
-    
+
+
+module Method2 = 
+   (*
+    * suppose x is the target factor
+    * Obtain multiples by generating a factor list in range [0,S) where S = MAX mod x
+    * and then multiply them with x
+    *)
+    begin
+
+    let getSum x =
+        let a = (MAX - 1) / x // get the maximum factor S, using (MAX - 1) to make sure a < S: see below
+        [0..a]// list includes a, so a<S is required
+        |>List.map(fun i -> i*x)
+        //|>List.map(fun x -> printfn "%A\n" x; x)
+        |>List.sum
+
+    let solve =
+        getSum 3 + getSum 5 - getSum (3*5)
+
+    end
 
 [<EntryPoint>]
 let main argv =
     printfn "%A\n" Method1.solve
-
+    printfn "%A\n" Method2.solve
     0 // return an integer exit code
