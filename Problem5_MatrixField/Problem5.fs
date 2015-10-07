e050086a5ea5766a0aa792d430f35276d7c8ddad
MatrixField <david.xelnaga@live.com>
2015-10-06 20:28:02 -0700

    Added Method2 to Problem5; Added a playGround

--- Problem5_MatrixField/Problem5.fs
+++ Problem5_MatrixField/Problem5.fs
@@ -35,13 +35,83 @@ module Method1 =
         |_ when exp = 0I -> 1I
         |_ -> loop 1I exp
 
-    let solve =
+    let solve x =
         let rec loop result = function
             |[] -> result
             |(factor, power)::tail -> loop (result * (pow factor power)) tail
         loop 1I lcm
 
+module Method2 =
+    // using greatest common diviser
+
+    let min = Method1.min
+    let max = Method1.max
+    //let max = 2I ** 64
+
+    let rec gcd = function
+        |(a,b) when b = 0I -> a
+        |(a,b) -> gcd(b, a%b)
+
+    let lcm = function
+        |(a,b) when a<b -> a*(b/gcd(b,a))
+        |(a,b) -> b*(a/gcd(a,b))
+
+    let arrLcm arr =
+        let rec loop = function
+            |[|a;b|] -> lcm(a,b)
+            |[|a|] -> a
+            |[||] -> raise (System.ArgumentException "null array provided to calculate lcm")
+            |arr ->
+                arr
+                |>Array.splitInto 2
+                |>Array.Parallel.map loop
+                |>loop
+        loop arr
+
+    let lstLcm lst =
+        let rec loop result = function
+            |head::tail -> loop (lcm(result,head)) tail
+            |[]->result
+        loop 1I lst
+
+    let solve x = arrLcm [|min..max|]
+
+let time func vars =
+    let t1 = System.DateTime.Now
+
+    let result = func vars
+
+    let t2 = System.DateTime.Now
+
+    (result, t2-t1)
+
+let writeLine path content = System.IO.File.AppendAllText(path,content)
+
 [<EntryPoint>]
 let main argv =
-    printfn "%A" Method1.solve
+    //printfn "%A" (Method1.solve 0)
+
+//    let (a,t) = time (Method2.lcm) (12I,18I)
+//
+//    printfn "%A %Ams" a t.TotalMilliseconds
+
+//    let (a,t) = time (Method2.solve) 0
+//
+//    printfn "%A %Ams" a t.TotalMilliseconds
+
+    for i in [1..128] do
+        let arr = [|1I.. 2I**i|]
+
+        let(_, tarr) = time (Method2.arrLcm) arr
+
+        let lst = arr|>Array.toList
+
+        let(_, tlst) = time (Method2.lstLcm) lst
+
+        printfn "arr:%A ms  lst:%A ms" tarr.TotalMilliseconds tlst.TotalMilliseconds
+
+        let output = "i=" + (string)arr.LongLength + "\t" + (string)tarr.TotalMilliseconds + "\t" + (string)tlst.TotalMilliseconds + "\n"
+
+        writeLine "Log.txt" output
+
     0 // return an integer exit code
