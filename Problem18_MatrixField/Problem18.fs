a7879d2505a043cf146c177f8888daa85dfd6383
MatrixField <david.xelnaga@live.com>
2015-10-29 00:49:24 -0700

    Solved problem 67

--- Problem18_MatrixField/Problem18.fs
+++ Problem18_MatrixField/Problem18.fs
@@ -2,31 +2,31 @@
 
 (*
  *By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
-
-3
-7 4
-2 4 6
-8 5 9 3
-
-That is, 3 + 7 + 4 + 9 = 23.
-
-Find the maximum total from top to bottom of the triangle below:
-
-75
-95 64
-17 47 82
-18 35 87 10
-20 04 82 47 65
-19 01 23 75 03 34
-88 02 77 73 07 63 67
-99 65 04 28 06 16 70 92
-41 41 26 56 83 40 80 70 33
-41 48 72 33 47 32 37 16 94 29
-53 71 44 65 25 43 91 52 97 51 14
-70 11 33 28 77 73 17 78 39 68 17 57
-91 71 52 38 17 14 91 43 58 50 27 29 48
-63 66 04 68 89 53 67 30 73 16 69 87 40 31
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+ *
+ *3
+ *7 4
+ *2 4 6
+ *8 5 9 3
+ *
+ *That is, 3 + 7 + 4 + 9 = 23.
+ *
+ *Find the maximum total from top to bottom of the triangle below:
+ *
+ *75
+ *95 64
+ *17 47 82
+ *18 35 87 10
+ *20 04 82 47 65
+ *19 01 23 75 03 34
+ *88 02 77 73 07 63 67
+ *99 65 04 28 06 16 70 92
+ *41 41 26 56 83 40 80 70 33
+ *41 48 72 33 47 32 37 16 94 29
+ *53 71 44 65 25 43 91 52 97 51 14
+ *70 11 33 28 77 73 17 78 39 68 17 57
+ *91 71 52 38 17 14 91 43 58 50 27 29 48
+ *63 66 04 68 89 53 67 30 73 16 69 87 40 31
+ *04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
 *)
 
 open System.Collections.Generic
@@ -116,14 +116,19 @@ module Method1 =
     /// function to find local maximum
     /// yield an array containing the local max whose root are nodes in the top
     let combine top bottem =
-        if (bottem|>Array.isEmpty) then top
+        if (bottem|>Array.isEmpty) then 
+            top
+            |>Array.map (fun x-> x,[x])
         else
             [|
             for i in[0..(top.Length-1)] do
-                if bottem.[i]>bottem.[i+1] then
-                    yield bottem.[i]+top.[i]
+                let a,alst = bottem.[i]
+                let b,blst = bottem.[i+1]
+                let c = top.[i]
+                if a>b then
+                    yield a+c, (c::alst)
                 else
-                    yield bottem.[i+1]+top.[i]
+                    yield b+c, (c::blst)
             |]
 
     /// calculate the maximum path sum in a number triangle
@@ -146,7 +151,8 @@ module Method1 =
 
 [<EntryPoint>]
 let main argv =
-    let r,t = Misc.Chrono.timeQuick Method1.solve text
+    let (r,lst),t = Misc.Chrono.timeQuick Method1.solve text
     printfn "The sum is %d, time used: %Ams" r t.TotalMilliseconds
+    printfn "%A" lst
     0 // return an integer exit code
 
---
+++ Problem67_MatrixField/Problem67.fs
@@ -0,0 +1,29 @@
+﻿//https://projecteuler.net/problem=67
+
+(*
+ *By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+ *
+ *3
+ *7 4
+ *2 4 6
+ *8 5 9 3
+ *
+ *That is, 3 + 7 + 4 + 9 = 23.
+ *
+ *Find the maximum total from top to bottom in triangle.txt
+*)
+
+open Problem18
+
+let solve _ =
+    System.IO.File.ReadAllLines("triangle.txt")
+    |>Array.Parallel.map (Misc.Text.ListParser.breakWhiteSpace)
+    |>Method1.makeHeap
+    |>Method1.maxPathSumOf
+
+[<EntryPoint>]
+let main argv = 
+    let (r,lst),t = Misc.Chrono.timeQuick solve ()
+    printfn "The sum is %d, time used: %Ams" r t.TotalMilliseconds
+    printfn "%A" lst
+    0 // return an integer exit code
